239d9cc721e06941cbbfa01c47dba4d825e14de4
jmigsdj <miggydj@gmail.com>
2019-02-21 09:48:47 +0800

    (+) Added Babel standalone for jsx to work (+) Practice Components (+) Test CSS on Components

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+function MyInfo(){
+  return(
+    <div className="info">
+      <h1>Miguel De Jesus</h1>
+      <p>Software Engineer</p>
+      <ul>
+        <li>Japan</li>
+        <li>Singapore</li>
+        <li>Philippines</li>
+      </ul>
+    </div>
+  )
+}
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"))
+ReactDOM.render(
+  <MyInfo />,
+  document.getElementById('root')
+);
